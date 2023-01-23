10702f955f09b68e71b590a97ef6937c0b020da3
yuran-nhassengo <yurannhassengo@gmail.com>
2023-01-23 15:21:53 +0200

    pratica

--- projecto1/src/App.js
+++ projecto1/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ const name ='Lamarck'
+
+ const newName = name.toUpperCase()
+
+ function sum(a,b){
+
+    return a+b  
+ }
+
+ const url ='https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Ola, {newName}</p>
+      <p>Soma: {sum(4,6)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
-  );
+  )
 }
 
 export default App;
